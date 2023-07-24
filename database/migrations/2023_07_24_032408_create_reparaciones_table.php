764e5d1819eb4be6d0a1658fd3728fd79761aadd
vBlackBearr <Luispuentes.pgo@outlook.com>
2023-07-23 21:29:00 -0600

    Migracion de reparaciones agregada

---
+++ database/migrations/2023_07_24_032408_create_reparaciones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reparaciones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('coche_id')->unsigned();
+            $table->foreign('coche_id')->references('id')->on('Coche');
+            $table->date('fecha_reparacion');
+            $table->decimal('horas');
+            $table->string('descripcion');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reparaciones');
+    }
+};
