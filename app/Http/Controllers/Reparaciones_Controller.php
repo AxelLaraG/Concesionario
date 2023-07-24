fb8793f18542ef80de4b4bf04602fba30c3b2c4a
vBlackBearr <Luispuentes.pgo@outlook.com>
2023-07-23 21:32:30 -0600

    Controlador Reparaciones Incompleto (para pruebas)

--- app/Http/Controllers/Reparaciones_Controller.php
+++ app/Http/Controllers/Reparaciones_Controller.php
@@ -3,12 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reparaciones;
+use App\Models\Coche;
 
 class Reparaciones_Controller extends Controller
 {
     public function index()
     {
-        //$coch = Coche::where('status', 1)->orderBy('precio', 'asc')->get();
-        return view('Reparaciones.index');
+        $reparaciones = Reparaciones::where('status', 1)->get();
+        return view('Reparaciones.index')->with('reparaciones',$reparaciones);
+    }
+    public function create()
+    {
+        return view('Reparaciones.create');
+    }
+    public function store(Request $request)
+    {
+        Reparaciones::create($request->all() + ['status' => 1]);
+        return redirect('/Reparaciones');
+    }
+    public function show(string $matricula)
+    {
+        $coche = Coche::where('matricula', $matricula)->first();
+        return view('Coche.read')->with('coche', $coche);
+    }
+    public function edit(string $matricula)
+    {
+        $coch = Coche::where('matricula', $matricula)->first();
+        return view('Coche.edit')->with('coche',$coch);
+    }
+    public function update(Request $request, string $matricula)
+    {
+        $datos = $request->all();
+        $coche = Coche::where('matricula', $matricula)->first();
+        $coche->update($datos);
+        return redirect('/Coche');
+    }
+    public function destroy(string $matricula)
+    {
+        $coche = Coche::where('matricula', $matricula)->first();
+        if($coche->tipo == 1){
+            CocheNuevo::where('coche_id',$coche->id)->update(['status' => 0]);
+        }else{
+            CocheUsado::where('coche_id',$coche->id)->update(['status' => 0]);
+        }
+
+        $coche::where('matricula',$matricula)->update(['status' => 0]);
+        
+        return redirect('/Coche');
     }
 }
