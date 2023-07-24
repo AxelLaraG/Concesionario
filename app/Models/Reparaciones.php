1ecb1c044baf93c5bab82c8e90535dda252b8ecd
vBlackBearr <Luispuentes.pgo@outlook.com>
2023-07-23 22:06:18 -0600

    Modelo Reparaciones

--- app/Models/Reparaciones.php
+++ app/Models/Reparaciones.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Reparaciones extends Model
 {
     use HasFactory;
+    protected $table='Reparaciones';
+    protected $fillable=['coche_id','fecha_reparacion','horas','descripcion','status']; 
+    
+    public function Coche()
+    {
+        return $this->belongsTo('App\Models\Coche','id','coche_id');
+    }
 }
