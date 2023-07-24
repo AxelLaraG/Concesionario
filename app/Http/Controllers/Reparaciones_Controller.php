3f68d3693080ed85c637f260d118fa0b64c96f45
vBlackBearr <Luispuentes.pgo@outlook.com>
2023-07-23 21:20:43 -0600

    x

--- app/Http/Controllers/Reparaciones_Controller.php
+++ app/Http/Controllers/Reparaciones_Controller.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class Reparaciones_Controller extends Controller
 {
-    //
+    public function index()
+    {
+        //$coch = Coche::where('status', 1)->orderBy('precio', 'asc')->get();
+        return view('Reparaciones.index');
+    }
 }
