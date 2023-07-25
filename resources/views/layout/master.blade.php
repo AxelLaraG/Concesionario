5bb884cadec5d617bf5ff0f00ab4eed39837cc99
aromerov117 <aromerov117@gmail.com>
2023-07-25 09:35:09 -0600

    op

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -33,8 +33,7 @@
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
                     data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Settings</a></li>
-                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                    <li><a class="dropdown-item" href="#!">Login</a></li>
                     <li>
                         <hr class="dropdown-divider" />
                     </li>
@@ -78,10 +77,6 @@
                             Mecanicos
                         </a>
                     </div>
-                </div>
-                <div class="sb-sidenav-footer">
-                    <div class="small">Logged in as:</div>
-                    Start Bootstrap
                 </div>
             </nav>
         </div>
